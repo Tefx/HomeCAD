eb2aab44f8b7540993e4e6d5660cf0c2ec516b34
tefx <zhaomeng.zhu@gmail.com>
2024-12-28 20:05:06 +0800

    improve shelf hole size

--- models/shelf_hole_cover_v2.py
+++ models/shelf_hole_cover_v2.py
@@ -4,13 +4,13 @@ from build123d import *
 from ocp_vscode import *
 
 
-CYL_DIAMETER = 20 * MM
-CYL_INNER_DIAMETER = 14.1 * MM
-HOLE_INSIDE_DIAMETER = 6.5 * MM
-HOLE_OUTER_DIAMETER_VERT = 18 * MM
-THICKNESS = 0.6 * MM
+CYL_DIAMETER = 21 * MM
+CYL_INNER_DIAMETER = 16 * MM
+HOLE_INSIDE_DIAMETER = 7 * MM
+HOLE_OUTER_DIAMETER_VERT = 20 * MM
+THICKNESS = 0.5 * MM
 SOCKET_THICKNESS = 1 * MM
-ERROR = 0.1 * MM
+TOLERANCE = 0.1 * MM
 SOCKET_SHIFT = 0.1
 
 CYL_RADIUS = CYL_DIAMETER / 2
@@ -23,14 +23,18 @@ HOLE_INSIDE_RADIUS = HOLE_INSIDE_DIAMETER / 2
 def make_inner(radius):
     with BuildPart() as part:
         with Locations((0, CYL_RADIUS * SOCKET_SHIFT, 0)):
-            Cylinder(radius, CYL_RADIUS, rotation=(90, 0, 0),
-                     align=(Align.CENTER, Align.CENTER, Align.MAX))
+            Cylinder(
+                radius,
+                CYL_RADIUS,
+                rotation=(90, 0, 0),
+                align=(Align.CENTER, Align.CENTER, Align.MAX),
+            )
         Cylinder(CYL_RADIUS - THICKNESS / 2, radius * 2, mode=Mode.INTERSECT)
     return part.part
 
 
 def make_cover():
-    with BuildPart() as part_cover:
+    with BuildPart() as part:
         Cylinder(
             CYL_RADIUS,
             HOLE_OUTER_RADIUS_VERT * 4,
@@ -44,17 +48,23 @@ def make_cover():
                 HOLE_OUTER_RADIUS_VERT,
             )
         thicken(amount=CYL_DIAMETER, both=True, mode=Mode.INTERSECT)
-    return part_cover.part
-        
+        # chamfer(part.edges()[1],
+        #         0.9 * MM,
+        #         angle=60,
+        #         )
+    return part.part
+
+
 def make_socket(radius):
-    with BuildPart() as part_cover:
-        length = CYL_DIAMETER * (1-SOCKET_SHIFT*2)
+    with BuildPart() as part:
+        length = CYL_DIAMETER * (1 - SOCKET_SHIFT * 2)
         Cylinder(radius, length)
         Cylinder(radius - SOCKET_THICKNESS, length, mode=Mode.SUBTRACT)
-    return part_cover.part
+    return part.part
 
-part_inner = make_inner(HOLE_INSIDE_RADIUS - SOCKET_THICKNESS - ERROR)
-part_cover = part_inner + make_cover()
+
+part_inner = make_inner(HOLE_INSIDE_RADIUS - SOCKET_THICKNESS - TOLERANCE)
+part_cover = (part_inner + make_cover()).rotate(Axis.Z, 180)
 part_socket = make_socket(HOLE_INSIDE_RADIUS)
 cover = pack([part_cover, copy(part_cover), part_socket], 10 * MM, align_z=True)
 
